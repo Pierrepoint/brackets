9832ee5ab515ff7f9b83e98b6fa6f454bd84a57f
Orlando <drapezoid@gmail.com>
2023-02-01 23:01:58 +0300

    update task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function check(str, bracketsConfig) {
+    let arr = Array.from(str);
+    let lengthBefore;
+    let lengthAfter;
+    let i = 0;
+    function analisys(bracketsConfig){
+        bracketsConfig.forEach(element => {
+            if ((arr[i] == element[0]) && (arr[i + 1] == element[1])){
+                if(arr.length == 2){arr = []}
+                else{arr = arr.slice(0,i).concat(arr.slice(i+2))}; 
+              i = 0;
+              lengthAfter = arr.length;
+            };
+          })
+    };
+
+    if(arr.length % 2 != 0){return false}
+    else{
+    while ( i <= arr.length - 2){
+      lengthBefore = arr.length;
+      analisys(bracketsConfig);
+      if(lengthAfter < lengthBefore){i = 0}
+        if(arr.length == 2){analisys(bracketsConfig)};
+      if(arr.length == 0){return true}
+      else if((arr.length != 0) && (i == arr.length - 2)){return false}
+      else i++;
+    }
+  }
 }
+module.exports = check;
